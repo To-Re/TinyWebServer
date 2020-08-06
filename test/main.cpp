748903f250c01227849b7f98fd8d71ca4ef16226
To-Re <541536515@qq.com>
2020-08-06 17:18:18 +0800

    修改编译环境配置信息

--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main () {
+    int a = 5;
+    cout << a << endl;
+    a = 10;
+    cout << a << endl;
+    cout << "this test0" << endl;
     cout << "this test1" << endl;
     return 0;
 }
