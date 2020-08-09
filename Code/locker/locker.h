0f173e9b43918a7d576c6bab00cec39411dc19ca
To-Re <541536515@qq.com>
2020-08-09 21:40:15 +0800

    重构 locker.h

--- Code/locker/locker.h
+++ Code/locker/locker.h
@@ -6,9 +6,12 @@
 #include <semaphore.h>
 
 /*
-封装了，信号量、互斥锁、条件变量，还未测试。拷贝控制成员可能有较大问题。
+封装了，信号量、互斥锁
+条件变量不封装了，感觉直接使用比较方便，还可以避免虚假唤醒（菜到不知道如何封装）
 感觉应该使用智能指针，但本人能力有限，还是先学习其他人的一些方法
 */
+
+namespace wzy {
 
 // 信号量
 class sem {
@@ -34,51 +37,30 @@ private:
 };
 
 // 互斥锁
-class cond;
-class locker {
+class uniqueLocker {
 public:
-    friend class cond;
-    locker() {
+    uniqueLocker() {
         if(pthread_mutex_init(&my_mutex, NULL)) {
             throw std::exception();
         }
+        pthread_mutex_lock(&my_mutex);
     }
-    ~locker() {
+    explicit uniqueLocker(pthread_mutex_t &mutex) : my_mutex(mutex) {
+        if(pthread_mutex_init(&my_mutex, NULL)) {
+            throw std::exception();
+        }
+        pthread_mutex_lock(&my_mutex);
+    }
+    ~uniqueLocker() {
+        pthread_mutex_unlock(&my_mutex);
         pthread_mutex_destroy(&my_mutex);
     }
-    locker(const locker &) = default;
-    locker& operator= (const locker&) = default; // 有线程阻塞时，被赋值可能出现问题
-    bool lock() {
-        return pthread_mutex_lock(&my_mutex) == 0;
-    }
-    bool unlock() {
-        return pthread_mutex_unlock(&my_mutex) == 0;
-    }
+    uniqueLocker(const uniqueLocker &) = delete;
+    uniqueLocker& operator= (const uniqueLocker&) = delete;
 private:
     pthread_mutex_t my_mutex;
 };
 
-// 条件变量
-class cond {
-public:
-    cond() {
-        if(pthread_cond_init(&my_cond, NULL)) {
-            throw std::exception();
-        }
-    }
-    ~cond() {
-        pthread_cond_destroy(&my_cond);
-    }
-    cond(const cond &) = default;
-    cond& operator= (const cond &) = default; // 有线程阻塞时，被赋值可能出现问题
-    bool wait(locker &my_lock) {
-        return pthread_cond_wait(&my_cond, &my_lock.my_mutex) == 0;
-    }
-    bool signal() {
-        return pthread_cond_signal(&my_cond) == 0;
-    }
-private:
-    pthread_cond_t my_cond;
-};
+}
 
 #endif
