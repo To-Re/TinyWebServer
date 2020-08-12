3859fe81658c729b2234a6eb0633c9031b7baf20
To-Re <541536515@qq.com>
2020-08-12 22:34:46 +0800

    add webserver 类

---
+++ Code/webserver/webserver.cpp
@@ -0,0 +1,81 @@
+#include "webserver.h"
+
+namespace wzy {
+
+webserver::webserver(
+    size_t port,
+    size_t workThreadNum,
+    size_t taskQueueNum,
+    size_t epollSize,
+    size_t maxFd,
+    int backlog,
+    std::string homePage
+) : PORT(port),  WORK_THREAD_NUM(workThreadNum), TASK_QUEUE_NUM(taskQueueNum),
+    EPOLL_SIZE(epollSize), MAX_FD(maxFd),
+    MAX_BACKLOG(backlog), HOME_PAGE(homePage),
+    ser(), clnt() {
+    // new epoll
+    ep = std::make_shared<wzy::etEpoll>(EPOLL_SIZE);
+    // new threadPool
+    thrdpool = std::make_shared< threadPool<httpcon> >(WORK_THREAD_NUM, TASK_QUEUE_NUM);
+    // 如果是相对路径
+    if(HOME_PAGE.empty() || HOME_PAGE[0] != '/') {
+        char buffer[200] = {0};
+        getcwd(buffer, sizeof(buffer));
+        std::string tmp(buffer);
+        HOME_PAGE = tmp + HOME_PAGE;
+    }
+
+    // 空或者末尾没有 '/'
+    if(HOME_PAGE.empty() || HOME_PAGE.back() != '/')
+        HOME_PAGE += "/";
+    
+    // new http 对象 待
+}
+
+webserver::~webserver() = default;
+
+void webserver::listen() {
+    ser.socket();
+    ser.bind(PORT);
+    ser.listen(MAX_BACKLOG);
+    ser.setNonBlocking();
+}
+
+void webserver::dealClientConnction() {
+    while(ser.nonBlockingAccept(clnt)) {
+        if(clientCount >= MAX_FD) {
+            error_handling("server busy", false);
+            break;
+        }
+        clnt.setNonBlocking();
+        if(ep -> add(clnt, EPOLLIN|EPOLLET|EPOLLOUT)); {
+            std::cout << "connected client : " << clnt << std::endl;
+            ++clientCount;
+        }
+    }
+}
+
+void webserver::start() {
+    isStop = true;
+    ep -> create();
+    ep -> add(ser, EPOLLIN|EPOLLET);
+    while(!isStop) {
+        int cnt = ep -> wait(-1);
+        if(cnt == -1) break;
+        for(int i = 0; i < cnt; ++i) {
+            int sockfd = ep -> events[i].data.fd;
+            if(sockfd == ser) {
+                dealClientConnction();
+            }
+            else if( (ep -> events[i].events) & EPOLLIN) {
+                dealRead(sockfd);
+            }
+            else if( (ep -> events[i].events) & EPOLLOUT) {
+                dealWrite(sockfd);
+            }
+        }
+    }
+}
+
+}
---
+++ Code/webserver/webserver.h
@@ -0,0 +1,62 @@
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+
+#include <memory>
+#include <iostream>
+
+#include "config.h"
+#include "etEpoll.h"
+#include "tcpsocket.h"
+#include "threadpool.h"
+#include "httpcon.h"
+
+namespace wzy {
+
+class webserver {
+public:
+    webserver(
+        size_t port = config::PORT,
+        size_t workThreadNum = config::WORK_THREAD_NUM,
+        size_t taskQueueNum = config::TASK_QUEUE_NUM,
+        size_t epollSize = config::EPOLL_SIZE,
+        size_t maxFd = config::MAX_FD,
+        int backlog = config::MAX_BACKLOG,
+        std::string homePage = config::HOME_PAGE
+    );
+    ~webserver();
+    void listen();
+    void start();
+private:
+    static void error_handling(const std::string & message, const bool & isexit = true) {
+        std::cerr << message << std::endl;
+        if(isexit) exit(1);
+    }
+    void dealClientConnction();
+    void dealRead(int);
+    void dealWrite(int);
+
+private:
+    const size_t PORT;
+    const size_t WORK_THREAD_NUM;
+    const size_t TASK_QUEUE_NUM;
+    const size_t EPOLL_SIZE;
+    const size_t MAX_FD;
+    const int MAX_BACKLOG;
+    std::string HOME_PAGE;
+
+    // webserver 是否运行
+    bool isStop = false;
+    // 客户端数量
+    int clientCount = 0;
+    // 套接字
+    wzy::tcpSocket ser, clnt;
+    // epoll
+    std::shared_ptr<wzy::etEpoll> ep;
+    // 线程池
+    std::shared_ptr<threadPool<httpcon> > thrdpool;
+};
+
+}
+
+#endif
