25ad9b6e79e5fa9049d1dd7c7b5b09256c4a3d1f
To-Re <541536515@qq.com>
2020-08-12 22:30:26 +0800

    modify config.h

--- Code/config.h
+++ Code/config.h
@@ -1,6 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <string>
 #include <cstddef>
 
 namespace config {
@@ -15,6 +16,15 @@ namespace config {
 
     // epoll 事件接收范围
     const size_t EPOLL_SIZE = 11451;
+
+    // 最大任务数量及客户端文件描述符数量
+    const size_t MAX_FD = 65536 - 10;
+
+    // tcpSocket 请求等待队列长度
+    const int MAX_BACKLOG = 20;
+
+    // 支持绝对路径和相对路径
+    const std::string HOME_PAGE = "page";
 };
 
 #endif
