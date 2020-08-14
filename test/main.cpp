cee1cf359e0e2cc42346946fb97069a2b60250bc
To-Re <541536515@qq.com>
2020-08-14 17:31:04 +0800

    modify test

--- test/main.cpp
+++ test/main.cpp
@@ -1,29 +1,12 @@
-#include "locker.h"
-#include "threadpool.h"
-#include <unistd.h>
-#include <iostream>
-#include <functional>
+#include "webserver.h"
+
 using namespace std;
 
-
-void fk(int a, int b) {
-    cout << a << " + " << b << " = " << a + b << endl;
-}
-
-void md() {
-    cout << "wtf" << endl;
-}
-
 int main() {
-    cout << config::PORT << endl;
-    wzy::threadPool<function<void()> > a;
-    a.addTask( []{cout << "fuck" << endl;} );
-    a.addTask({bind(fk, 4, 6), bind(fk, 4, 6)});
-    a.addTask({bind(fk, 4, 6)});
-    a.addTask({md});
-    a.addTask(md);
-    a.addTask(md);
-    a.addTask(std::move(md));
-    sleep(2);
+    wzy::webserver server(10002);
+    //监听
+    server.listen();
+    //运行
+    server.start();
     return 0;
 }
