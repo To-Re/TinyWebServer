be8d1190bc8600b072c97ddb856c52e421438b1c
To-Re <541536515@qq.com>
2020-08-15 16:51:30 +0800

    add httpresponse

---
+++ Code/http/httpresponse.cpp
@@ -0,0 +1,52 @@
+#include "httpresponse.h"
+
+namespace wzy {
+
+std::map<int, std::string> httpresponse::state = {
+    {200, "OK"},
+    {400, "Bad Request"},
+    {403, "Forbidden"},
+    {404, "Not Found"}
+};
+
+std::map<int, std::string> httpresponse::merrorfile = {
+    {400, "400.html"},
+    {403, "403.html"},
+    {404, "404.html"},
+};
+
+void httpresponse::compose(outbuffer &out, bool is_400) {
+    // std::cout << "compose : " << std::endl;
+    // std::cout << HOME << std::endl;
+    // std::cout << is_400 << std::endl;
+    // std::cout << METHON << std::endl;
+    // std::cout << PATH << std::endl;
+    // std::cout << VERSION << std::endl;
+    out.clear();
+    int sta = 200;
+    if(is_400) sta = 400;
+    // 检查文件
+    check(sta, out);
+    out.add(VERSION + " " + std::to_string(sta) + " " + state[sta] + "\n\r" + "\n\r");
+    // 传输 PATH 对应文件
+    out.addfile(HOME+PATH);
+    out.coutall();
+}
+
+void httpresponse::check(int &sta, outbuffer &out) {
+    if(sta == 400) {
+        sta = 400;
+        PATH == merrorfile[sta];
+        return;
+    }
+    if(PATH == "/" || PATH.empty()) PATH = "index.html";
+    int len = out.getFileLen(HOME+PATH);
+    if(len == -1) {
+        sta = 404;
+        PATH == merrorfile[sta];
+        return;
+    }
+    sta = 200;
+}
+
+}
---
+++ Code/http/httpresponse.h
@@ -0,0 +1,33 @@
+#ifndef HTTPRESPONSE_H
+#define HTTPRESPONSE_H
+
+#include <string>
+#include <map>
+#include "buffer.h"
+
+namespace wzy {
+
+class httpresponse {
+public:
+    httpresponse() = default;
+    ~httpresponse() = default;
+    void init(const std::string & home, const std::string & methon, const std::string & path, const std::string & version) {
+        HOME = home;
+        METHON = methon;
+        PATH = path;
+        VERSION =version;
+    }
+    // 第一个参数传入 输出缓冲区
+    // 第二个参数传入 true 表示语法错误返回 400
+    void compose(outbuffer &, bool);
+private:
+    // 检查文件
+    void check(int &, outbuffer &);
+    static std::map<int, std::string> state;
+    static std::map<int, std::string> merrorfile;
+    std::string HOME, METHON, PATH, VERSION;
+};
+
+}
+
+#endif
